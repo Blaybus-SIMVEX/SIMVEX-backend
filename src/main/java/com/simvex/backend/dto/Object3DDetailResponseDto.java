acd690c56b87f0ae604ab79bbe4fc7969a909cd8
kimyongjun0129 <kimyongjun0129@naver.com>
2026-02-04 22:37:44 +0900

    feat(component): 3D 모델 전체 조회 & 단 건 상세 조회 기능 추가

---
+++ src/main/java/com/simvex/backend/dto/Object3DDetailResponseDto.java
@@ -0,0 +1,24 @@
+package com.simvex.backend.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class Object3DDetailResponseDto {
+    private Long id;
+    private String name;
+    private String nameEn;
+    private String description;
+    private String thumbnailUrl;
+    private String modelFileUrl;
+    private String category;
+    private ProductDescriptionDto productDescription;
+    private List<ComponentDto> components;
+}
