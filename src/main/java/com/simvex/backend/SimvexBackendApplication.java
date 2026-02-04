b0227349a1c9f1f534d7723ec56665df4cc56fde
kimyongjun0129 <kimyongjun0129@naver.com>
2026-02-04 22:33:13 +0900

    first commit

---
+++ src/main/java/com/simvex/backend/SimvexBackendApplication.java
@@ -0,0 +1,12 @@
+package com.simvex.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimvexBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SimvexBackendApplication.class, args);
+    }
+}
